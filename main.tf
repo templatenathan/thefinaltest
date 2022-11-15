6490bd1c9c275cf3f4949b350c2e199554ffb36b
Nathan Vass <Nathanevass@gmail.com>
2022-11-15 16:10:44 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    sonarcloud = {
+      source  = "rewe-digital/sonarcloud"
+      version = "0.2.1"
+    }
+  }
+
+  cloud {
+    organization = "templatenathan"
+
+    workspaces {
+      name = "templatenathan"
+    }
+  }
+}
+
+resource "sonarcloud_project" "template" {
+  key        = "template"
+  name       = "template"
+  visibility = "public"
+}
